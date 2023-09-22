6d815653db5df5daa7553a4d50ab6257603571ec
Nam <nhnam2712@gmail.com>
2023-09-22 13:10:32 +0700

    Stories commit

---
+++ app/src/main/java/vn/edu/usth/instagram/StoriesActivity.java
@@ -0,0 +1,18 @@
+package vn.edu.usth.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class StoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stories);
+    }
+}
