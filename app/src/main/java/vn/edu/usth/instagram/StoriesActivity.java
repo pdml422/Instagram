2429a039677375f116fc6a8bf3d5f9fa1d8968d8
Admin <nhnam2712@gmail.com>
2023-09-25 22:15:31 +0700

    Delete Hardcode

--- app/src/main/java/vn/edu/usth/instagram/StoriesActivity.java
+++ app/src/main/java/vn/edu/usth/instagram/StoriesActivity.java
@@ -1,30 +1,19 @@
 package vn.edu.usth.instagram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StoriesActivity extends AppCompatActivity {
-
-    private ImageView close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
 
-        close = findViewById(R.id.close);
+        ImageView close = findViewById(R.id.close);
 
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        close.setOnClickListener(v -> finish());
     }
 }
