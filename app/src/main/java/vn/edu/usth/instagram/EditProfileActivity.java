65c9c572592695a632c2d64c6b3e692f2014e035
Admin <nhnam2712@gmail.com>
2023-09-27 02:06:19 +0700

    add DB

--- app/src/main/java/vn/edu/usth/instagram/EditProfileActivity.java
+++ app/src/main/java/vn/edu/usth/instagram/EditProfileActivity.java
@@ -1,31 +1,105 @@
 package vn.edu.usth.instagram;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.content.Intent;
-import android.view.View;
-import android.widget.ImageView;
-
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import vn.edu.usth.instagram.Fragment.ProfileFragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import vn.edu.usth.instagram.Model.User;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    private ImageView close;
+    private View imageProfile;
+    private TextView save;
+    private TextView changePhoto;
+    private MaterialEditText fullname;
+    private MaterialEditText username;
+    private MaterialEditText bio;
+
+    private FirebaseUser fUser;
+
+    private Uri mImageUri;
+    private StorageTask uploadTask;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        //close icon
+        ImageView close = findViewById(R.id.close);
+        close.setOnClickListener(v -> finish());
         close = findViewById(R.id.close);
 
-        close.setOnClickListener(new View.OnClickListener() {
+        //change info
+        imageProfile = findViewById(R.id.image_profile);
+        save = findViewById(R.id.save);
+        changePhoto = findViewById(R.id.change_photo);
+        fullname = findViewById(R.id.fullname);
+        username = findViewById(R.id.username);
+        bio = findViewById(R.id.bio);
+
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        storageRef = FirebaseStorage.getInstance().getReference().child("Uploads");
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(fUser.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                finish();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                fullname.setText(user.getName());
+                username.setText(user.getUsername());
+                bio.setText(user.getBio());
+                Picasso.get().load(user.getImageurl()).into((ImageView) imageProfile);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
+
+        close.setOnClickListener(v -> finish());
+
+        changePhoto.setOnClickListener(v -> ImagePicker.with(EditProfileActivity.this)
+                .crop()                    //Crop image(Optional), Check Customization for more option
+                .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                .start(101));
+
+        save.setOnClickListener(v -> updateProfile());
     }
 
+    private void updateProfile() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("fullname", fullname.getText().toString());
+        map.put("username", username.getText().toString());
+        map.put("bio", bio.getText().toString());
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(fUser.getUid()).updateChildren(map);
+    }
 }
