8007ec30419d4d75a26c314799f4cdd122c492b0
mp <minggphamm15@gmail.com>
2023-09-27 02:20:34 +0700

    add follow button in useritem.xml, add follow function in useradapter.

--- app/src/main/java/vn/edu/usth/instagram/Adapter/UserAdapter.java
+++ app/src/main/java/vn/edu/usth/instagram/Adapter/UserAdapter.java
@@ -4,12 +4,18 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -46,11 +52,49 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         User user  = mUsers.get(position);
+        holder.btnFollow.setVisibility(View.VISIBLE);
         holder.username.setText(user.getUsername());
         holder.fullname.setText(user.getName());
 
         Picasso.get().load(user.getImageurl()).placeholder(R.mipmap.ic_launcher).into(holder.imageProfile);
+        isFollowed(user.getId(), holder.btnFollow);
 
+        if (user.getId().equals(firebaseUser.getUid())){
+            holder.btnFollow.setVisibility(View.GONE);
+        }
+        holder.btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holder.btnFollow.getText().toString().equals(("follow"))){
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child((firebaseUser.getUid())).child("following").child(user.getId()).setValue(true);
+
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(user.getId()).child("followers").child(firebaseUser.getUid()).setValue(true);
+                } else {
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child((firebaseUser.getUid())).child("following").child(user.getId()).removeValue();
+
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(user.getId()).child("followers").child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+    }
+
+    private void isFollowed(final String id, Button btnFollow) {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid()).child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(id).exists())
+                    btnFollow.setText("following");
+                else
+                    btnFollow.setText("follow");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -63,6 +107,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
         public CircleImageView imageProfile;
         public TextView username;
         public TextView fullname;
+        public Button btnFollow;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -70,6 +115,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
             imageProfile = itemView.findViewById(R.id.image_profile);
             username = itemView.findViewById(R.id.username);
             fullname = itemView.findViewById(R.id.fullname);
+            btnFollow = itemView.findViewById(R.id.btn_follow);
         }
     }
 }
