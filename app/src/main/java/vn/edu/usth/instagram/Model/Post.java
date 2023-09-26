68d61b5ce22e69013264568b7ea1399de63559e8
Luong <luong.pdm2204@gmail.com>
2023-09-27 01:22:48 +0700

    backend for home post

---
+++ app/src/main/java/vn/edu/usth/instagram/Model/Post.java
@@ -0,0 +1,51 @@
+package vn.edu.usth.instagram.Model;
+
+public class Post {
+    private String description;
+    private String imageurl;
+    private String postId;
+    private String publisher;
+
+    public Post() {
+    }
+
+    public Post(String description, String imageurl, String postId, String publisher) {
+        this.description = description;
+        this.imageurl = imageurl;
+        this.postId = postId;
+        this.publisher = publisher;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+}
